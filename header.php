1a602fc4266bca43cb0f2da4757e8f84fa51b852
Atsushi_Yoshino <atsushi.yoshino.0705@gmail.com>
2020-06-15 15:46:49 +0900

    雛形作成

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?> /css/reset.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?> /css/style.css">
+
+  <?php wp_head();?>
+</head>
+<body>
+<head>
+
+</head>
