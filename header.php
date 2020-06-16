cc9b8ddcc9d508faa2bfe5bd50faa7bd3a5d3b9d
森薗美月 <attesa.1.mzk@gmail.com>
2020-06-16 15:45:16 +0900

    ヘッダー、フッター、メインビジュアル完成

--- header.php
+++ header.php
@@ -1,15 +1,31 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?> /css/reset.css">
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?> /css/style.css">
+    <title>American Village</title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/reset.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/style.css">
 
   <?php wp_head();?>
 </head>
-<body>
-<head>
 
-</head>
+<body>
+<header>
+<ul class="nav">
+  <li><a href="#">Menu01</a></li>
+  <li><a href="#">Menu02</a></li>
+  <li><a href="#">Menu03</a></li>
+  <li><a href="#">Menu04</a></li>
+</ul>
+
+
+  <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="" class="imglogo">
+  <img src="<?php echo get_template_directory_uri(); ?>/img/visual_1_pc.png" alt=""  class="imgmain">
+  <img src="<?php echo get_template_directory_uri(); ?>/img/visual_text_pc.png" alt=""  class="imgcharset">
+
+
+<div class="explanation">
+   <p>進化し続ける「街」アメリカンビレッジマガジン</p>
+</div>
+</header>
