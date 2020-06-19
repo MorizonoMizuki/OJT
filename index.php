880078f8b78661b747acdb7fdb778d0f1b29b7bc
Atsushi_Yoshino <atsushi.yoshino.0705@gmail.com>
2020-06-19 13:47:26 +0900

    調整

--- index.php
+++ index.php
@@ -1,48 +1,44 @@
 <?php get_header(); ?>
 
-<div class="container">
-
-    <?php if (have_posts()) : ?>
-        
-        <p class="articles-title">Latest Articles</p>
-        
-        <ul class="articles">
-            
-            <?php while( have_posts() ): the_post(); ?>
-            
-            <li class="article">
+        <div>
+            <p class="articles-title">Latest Articles</p>
+        </div>
+           
+            <?php if (have_posts()) : ?>
                 
-                <?php if (has_post_thumbnail()): ?>
+                <ul class="articles inner">
                     
-                    <?php the_post_thumbnail('large'); ?>
+                    <?php while( have_posts() ): the_post(); ?>
                     
-                <?php else: ?>
+                    <li class="article">
                         
-                    <img src="https://placehold.jp/150x150.png" alt="">
+                        <?php if (has_post_thumbnail()): ?>
+                            
+                            <?php the_post_thumbnail('large'); ?>
+                            
+                        <?php else: ?>
+                                
+                            <img src="https://placehold.jp/150x150.png" alt="">
+                                
+                        <?php endif; ?>
+                                
+                            <p><?php the_time("Y/m/d/");?></p>
+                            <p><?php the_excerpt(); ?></p>
+                            <div class="link">
+                                <a href="<?php the_permalink(); ?>" class="read-more Item"><span class="Item-Text">READ MORE</span></a>
+                            </div>
+                                
+                    </li>
+                            
+                    <?php endwhile; ?>
+                            
+                </ul>
                         
-                <?php endif; ?>
-                        
-                    <p><?php the_time("Y/m/d/");?></p>
-                    <p><?php the_excerpt(); ?></p>
-                    <div class="link">
-                        <a href="<?php the_permalink(); ?>" class="read-more Item"><span class="Item-Text">READ MORE</span></a>
-                    </div>
-                        
-            </li>
+            <?php else: ?>
+                            
+                <h2>最近の投稿はありません</h2>
+                            
+            <?php endif; ?>
                     
-            <?php endwhile; ?>
-                    
-        </ul>
-                
-    <?php else: ?>
-                    
-        <h2>最近の投稿はありません</h2>
-                    
-    <?php endif; ?>
-                    
-                    
-                
-                
-</div>
                 
 <?php get_footer();?>
